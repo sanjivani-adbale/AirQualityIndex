57921b419aa61491aee78abe4e13ea378bb1187b
sanjivani-adbale <sanjivadbale1971@gmail.com>
2025-11-01 16:32:52 +0530

    Update WebConfig.java

--- AirQualityService/src/main/java/com/airquality/config/WebConfig.java
+++ AirQualityService/src/main/java/com/airquality/config/WebConfig.java
@@ -9,9 +9,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200") // Angular's default development URL
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Add methods you use
-                .allowedHeaders("*") // Allow all headers
-                .allowCredentials(true); // If cookies are used
+                .allowedOrigins("http://localhost:4200") 
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") 
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
